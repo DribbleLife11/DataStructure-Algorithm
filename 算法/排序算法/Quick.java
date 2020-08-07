c7e5b0dd37b32d25fb7c0a9787bb9fecbeae3c24
杨智康 <55861791+DribbleLife11@users.noreply.github.com>
2020-08-07 22:44:57 +0800

    快速排序

---
+++ 算法/排序算法/Quick.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+* 快速排序
+*/
+public class Quick {
+
+    public static void quickSort(int[] a,int low,int high) {
+        if (low >= high) return;
+
+        int q =partition(a,low,high);
+        quickSort(a,low,q-1);
+        quickSort(a,q+1,high);
+    }
+    
+    //切分
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low;
+        for (int j = low; j <= high-1; j++) {
+            if (arr[j] < pivot) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
+}
